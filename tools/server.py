29fe94d101aab574202899583cc2fb8a22bdf1cf
dylanbeattie <dylan@dylanbeattie.net>
2019-03-24 12:40:04 +0000

    Tooling fixes & reformatting.

---
+++ tools/server.py
@@ -0,0 +1,14 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 1337
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    pass
+
+Handler.extensions_map['.rock'] = 'text/plain'
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()

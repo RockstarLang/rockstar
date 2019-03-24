826baa1d7a7142c5b2e52ba55817c1d902eee206
dylanbeattie <dylan@dylanbeattie.net>
2019-03-24 20:20:50 +0000

    Cleans up JS indentation

--- satriani/satriani.interpreter.js
+++ satriani/satriani.interpreter.js
@@ -31,7 +31,7 @@ Environment.prototype = {
         return this.vars[name] = value;
     },
 
-    run: function(program) {
+    run: function (program) {
         let result = evaluate(program, this);
         return (result ? result.value : undefined);
     },
@@ -39,222 +39,222 @@ Environment.prototype = {
     pronoun_alias: null,
 }
 
- function evaluate(tree, env) {
-     if (tree == MYSTERIOUS) return(undefined);
-     let list = Object.entries(tree)
-     for (let i = 0; i < list.length; i++) {
-         let node = list[i];
-         let type = node[0];
-         let expr = node[1];
-         switch (type) {
-             case "action": return(tree);
-             case "list":
-                 let result = null;
-                 for (let i = 0; i < expr.length; i++) {
-                     let next = expr[i];
-                     result = evaluate(next, env);
-                     if (result && result.action) return (result);
-                 }
-                 return result;
-             case "conditional":
-                 if (evaluate(expr.condition, env)) {
-                     return evaluate(expr.consequent, env);
-                 } else if (expr.alternate) {
-                     return evaluate(expr.alternate, env);
-                 }
-                 return;
-             case 'break':
-                 return { 'action' : 'break' };
-             case 'continue':
-                 return { 'action' : 'continue' };
-             case "return":
-                 return { 'action': 'return', 'value': evaluate(expr.expression, env) };
-             case "number":
-             case "string":
-             case "constant":
-                 return (expr);
-             case "output":
-                 let printable = evaluate(expr, env);
-                 if (typeof (printable) == 'undefined') printable = "mysterious";
-                 env.output(printable);
-                 return;
-             case "listen":
-                 return env.input();
-             case "binary":
-                 return binary(expr, env);
-             case "lookup":
-                 if (expr.variable.pronoun) return env.lookup(env.pronoun_alias);
-                 return env.lookup(expr.variable);
-             case "assign":
-                 let alias = "";
-                 let value = evaluate(expr.expression, env);
-                 if (expr.variable.pronoun) {
-                     alias = env.pronoun_alias;
-                 } else {
-                     alias = expr.variable;
-                     env.pronoun_alias = alias;
-                 }
-                 env.assign(alias, value);
-                 return;
-             case "pronoun":
-                 return env.lookup(env.pronoun_alias);
-             case "blank":
-                 return;
-             case "increment":
-                 let old_increment_value = env.lookup(expr.variable);
-                 switch (typeof (old_increment_value)) {
-                     case "boolean":
-                         if (expr.multiple % 2 != 0) env.assign(expr.variable, !old_increment_value);
-                         return;
-                     default:
-                         env.assign(expr.variable, (old_increment_value + expr.multiple));
-                         return;
-                 }
-                 return;
-             case "decrement":
-                 let old_decrement_value = env.lookup(expr.variable);
-                 switch (typeof (old_decrement_value)) {
-                     case "boolean":
-                         if (expr.multiple % 2 != 0) env.assign(expr.variable, !old_decrement_value);
-                         return;
-                     default:
-                         env.assign(expr.variable, (old_decrement_value - expr.multiple));
-                         return;
-                 }
-                 return;
+function evaluate(tree, env) {
+    if (tree == MYSTERIOUS) return (undefined);
+    let list = Object.entries(tree)
+    for (let i = 0; i < list.length; i++) {
+        let node = list[i];
+        let type = node[0];
+        let expr = node[1];
+        switch (type) {
+            case "action": return (tree);
+            case "list":
+                let result = null;
+                for (let i = 0; i < expr.length; i++) {
+                    let next = expr[i];
+                    result = evaluate(next, env);
+                    if (result && result.action) return (result);
+                }
+                return result;
+            case "conditional":
+                if (evaluate(expr.condition, env)) {
+                    return evaluate(expr.consequent, env);
+                } else if (expr.alternate) {
+                    return evaluate(expr.alternate, env);
+                }
+                return;
+            case 'break':
+                return { 'action': 'break' };
+            case 'continue':
+                return { 'action': 'continue' };
+            case "return":
+                return { 'action': 'return', 'value': evaluate(expr.expression, env) };
+            case "number":
+            case "string":
+            case "constant":
+                return (expr);
+            case "output":
+                let printable = evaluate(expr, env);
+                if (typeof (printable) == 'undefined') printable = "mysterious";
+                env.output(printable);
+                return;
+            case "listen":
+                return env.input();
+            case "binary":
+                return binary(expr, env);
+            case "lookup":
+                if (expr.variable.pronoun) return env.lookup(env.pronoun_alias);
+                return env.lookup(expr.variable);
+            case "assign":
+                let alias = "";
+                let value = evaluate(expr.expression, env);
+                if (expr.variable.pronoun) {
+                    alias = env.pronoun_alias;
+                } else {
+                    alias = expr.variable;
+                    env.pronoun_alias = alias;
+                }
+                env.assign(alias, value);
+                return;
+            case "pronoun":
+                return env.lookup(env.pronoun_alias);
+            case "blank":
+                return;
+            case "increment":
+                let old_increment_value = env.lookup(expr.variable);
+                switch (typeof (old_increment_value)) {
+                    case "boolean":
+                        if (expr.multiple % 2 != 0) env.assign(expr.variable, !old_increment_value);
+                        return;
+                    default:
+                        env.assign(expr.variable, (old_increment_value + expr.multiple));
+                        return;
+                }
+                return;
+            case "decrement":
+                let old_decrement_value = env.lookup(expr.variable);
+                switch (typeof (old_decrement_value)) {
+                    case "boolean":
+                        if (expr.multiple % 2 != 0) env.assign(expr.variable, !old_decrement_value);
+                        return;
+                    default:
+                        env.assign(expr.variable, (old_decrement_value - expr.multiple));
+                        return;
+                }
+                return;
 
-             case "while_loop":
-                 while_outer: while (evaluate(expr.condition, env)) {
-                     let result = evaluate(expr.consequent, env);
-                     if (result) switch(result.action) {
-                         case 'continue':
-                             continue while_outer;
-                         case 'break':
-                             break while_outer;
-                         case 'return':
-                             return (result);
-                     }
-                 }
-                 return;
-             case "until_loop":
-                 until_outer: while (!evaluate(expr.condition, env)) {
-                     let result = evaluate(expr.consequent, env);
-                     if (result) switch(result.action) {
-                         case 'continue':
-                             continue until_outer;
-                         case 'break':
-                             break until_outer;
-                         case 'return':
-                             return (result);
-                     }
-                 }
-                 return;
-             case "comparison":
-                 let lhs = evaluate(expr.lhs, env);
-                 let rhs = evaluate(expr.rhs, env);
-                 switch (expr.comparator) {
-                     case "eq":
-                         return eq(lhs, rhs);
-                     case "ne":
-                         return !eq(lhs, rhs);
-                     case "lt":
-                         return (lhs < rhs);
-                     case "le":
-                         return (lhs <= rhs);
-                     case "ge":
-                         return (lhs >= rhs);
-                     case "gt":
-                         return (lhs > rhs);
-                 }
-             case "and":
-                 return (evaluate(expr.lhs, env) && evaluate(expr.rhs, env));
-             case "nor":
-                 return (!evaluate(expr.lhs, env) && !evaluate(expr.rhs, env));
-             case "or":
-                 return (evaluate(expr.lhs, env) || evaluate(expr.rhs, env));
-             case "not":
-                 return (!evaluate(expr.expression, env));
-             case "function":
-                 env.assign(expr.name, make_lambda(expr, env));
-                 return;
-             case "call":
-                 let func = env.lookup(expr.name);
-                 let func_result = func.apply(null, expr.args.map(arg => evaluate(arg, env)));
-                 return (func_result ? func_result.value : undefined);
-             default:
-                 throw new Error("Sorry - I don't know how to evaluate this: " + JSON.stringify(tree))
+            case "while_loop":
+                while_outer: while (evaluate(expr.condition, env)) {
+                    let result = evaluate(expr.consequent, env);
+                    if (result) switch (result.action) {
+                        case 'continue':
+                            continue while_outer;
+                        case 'break':
+                            break while_outer;
+                        case 'return':
+                            return (result);
+                    }
+                }
+                return;
+            case "until_loop":
+                until_outer: while (!evaluate(expr.condition, env)) {
+                    let result = evaluate(expr.consequent, env);
+                    if (result) switch (result.action) {
+                        case 'continue':
+                            continue until_outer;
+                        case 'break':
+                            break until_outer;
+                        case 'return':
+                            return (result);
+                    }
+                }
+                return;
+            case "comparison":
+                let lhs = evaluate(expr.lhs, env);
+                let rhs = evaluate(expr.rhs, env);
+                switch (expr.comparator) {
+                    case "eq":
+                        return eq(lhs, rhs);
+                    case "ne":
+                        return !eq(lhs, rhs);
+                    case "lt":
+                        return (lhs < rhs);
+                    case "le":
+                        return (lhs <= rhs);
+                    case "ge":
+                        return (lhs >= rhs);
+                    case "gt":
+                        return (lhs > rhs);
+                }
+            case "and":
+                return (evaluate(expr.lhs, env) && evaluate(expr.rhs, env));
+            case "nor":
+                return (!evaluate(expr.lhs, env) && !evaluate(expr.rhs, env));
+            case "or":
+                return (evaluate(expr.lhs, env) || evaluate(expr.rhs, env));
+            case "not":
+                return (!evaluate(expr.expression, env));
+            case "function":
+                env.assign(expr.name, make_lambda(expr, env));
+                return;
+            case "call":
+                let func = env.lookup(expr.name);
+                let func_result = func.apply(null, expr.args.map(arg => evaluate(arg, env)));
+                return (func_result ? func_result.value : undefined);
+            default:
+                throw new Error("Sorry - I don't know how to evaluate this: " + JSON.stringify(tree))
 
-         }
-     }
- }
+        }
+    }
+}
 
- function eq(lhs, rhs) {
-     if (typeof(lhs) == 'undefined') return(typeof(rhs) == 'undefined');
-     if (typeof(rhs) == 'undefined') return(typeof(lhs) == 'undefined');
+function eq(lhs, rhs) {
+    if (typeof (lhs) == 'undefined') return (typeof (rhs) == 'undefined');
+    if (typeof (rhs) == 'undefined') return (typeof (lhs) == 'undefined');
 
-     if (typeof(lhs) == 'boolean') return(eq_boolean(lhs, rhs));
-     if (typeof(rhs) == 'boolean') return(eq_boolean(rhs, lhs));
-     if (typeof(lhs) == 'number') return(eq_number(lhs, rhs));
-     if (typeof(rhs) == 'number') return(eq_number(rhs, lhs));
+    if (typeof (lhs) == 'boolean') return (eq_boolean(lhs, rhs));
+    if (typeof (rhs) == 'boolean') return (eq_boolean(rhs, lhs));
+    if (typeof (lhs) == 'number') return (eq_number(lhs, rhs));
+    if (typeof (rhs) == 'number') return (eq_number(rhs, lhs));
 
     return lhs == rhs;
- }
+}
 
- function eq_number(number, other) {
-    if (other == null || typeof(other) == 'undefined') return(number === 0);
-    return(other == number);
- }
+function eq_number(number, other) {
+    if (other == null || typeof (other) == 'undefined') return (number === 0);
+    return (other == number);
+}
 
- function eq_boolean(bool, other) {
+function eq_boolean(bool, other) {
     // false equals null in Rockstar
     if (other == null) other = false;
     // false equals zero in Rockstar
-    if(typeof(other) == 'number') other = (other !== 0);
-    if (typeof(other) == 'string') other = (other !== "");
+    if (typeof (other) == 'number') other = (other !== 0);
+    if (typeof (other) == 'string') other = (other !== "");
     return (bool == other);
- }
+}
 
- function make_lambda(expr, env) {
-     function lambda() {
-         let names = expr.args;
-         if (names.length != arguments.length) throw('Wrong number of arguments supplied to function ' + expr.name + ' (' + expr.args + ')');
-         let scope = env.extend();
-         for (let i = 0; i < names.length; ++i) scope.def(names[i], arguments[i])
-         return evaluate(expr.body, scope);
-     }
+function make_lambda(expr, env) {
+    function lambda() {
+        let names = expr.args;
+        if (names.length != arguments.length) throw ('Wrong number of arguments supplied to function ' + expr.name + ' (' + expr.args + ')');
+        let scope = env.extend();
+        for (let i = 0; i < names.length; ++i) scope.def(names[i], arguments[i])
+        return evaluate(expr.body, scope);
+    }
 
-     return lambda;
- }
+    return lambda;
+}
 
- function binary(b, env) {
-     let l = evaluate(b.lhs, env);
-     let r = evaluate(b.rhs, env);
-     if (typeof(l) == 'undefined') l = 'mysterious';
-     if (typeof(r) == 'undefined') r = 'mysterious'
-     switch (b.op) {
-         case '+':
-             return l + r;
-         case '-':
-             return l - r;
-         case '/':
-             return l / r;
-         case '*':
-             return multiply(l, r);
-     }
- }
- function multiply(lhs, rhs) {
+function binary(b, env) {
+    let l = evaluate(b.lhs, env);
+    let r = evaluate(b.rhs, env);
+    if (typeof (l) == 'undefined') l = 'mysterious';
+    if (typeof (r) == 'undefined') r = 'mysterious'
+    switch (b.op) {
+        case '+':
+            return l + r;
+        case '-':
+            return l - r;
+        case '/':
+            return l / r;
+        case '*':
+            return multiply(l, r);
+    }
+}
+function multiply(lhs, rhs) {
     // Null, nothing, noone, nowhere, etc. are all zero for multiplication purposes.
     if (rhs == null) rhs = 0;
     if (lhs == null) lhs = 0;
-     // Multiplying numbers just works.
-     if (typeof (lhs) == 'number' && typeof (rhs) == 'number') return (lhs * rhs);
-     // Multiplying strings by numbers does repeated concatenation
-     if (typeof (lhs) == 'string' && typeof (rhs) == 'number') return multiply_string(lhs, rhs);
-     if (typeof (lhs) == 'number' && typeof (rhs) == 'string') return multiply_string(rhs, lhs);
- }
+    // Multiplying numbers just works.
+    if (typeof (lhs) == 'number' && typeof (rhs) == 'number') return (lhs * rhs);
+    // Multiplying strings by numbers does repeated concatenation
+    if (typeof (lhs) == 'string' && typeof (rhs) == 'number') return multiply_string(lhs, rhs);
+    if (typeof (lhs) == 'number' && typeof (rhs) == 'string') return multiply_string(rhs, lhs);
+}
 
- function multiply_string(s, n) {
+function multiply_string(s, n) {
     let result = Array();
-    while(--n >= 0) result.push(s);
-    return(result.join(''));
+    while (--n >= 0) result.push(s);
+    return (result.join(''));
 }

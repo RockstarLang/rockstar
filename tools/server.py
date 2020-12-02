edb9281955001e3482545fd3a8de06148b5d5a8c
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-02 15:41:51 +0000

    Fixed console output in tools/server.py

--- tools/server.py
+++ tools/server.py
@@ -10,5 +10,5 @@ Handler.extensions_map['.rock'] = 'text/plain'
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print ("serving at port", PORT)
 httpd.serve_forever()

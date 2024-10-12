3de39b8b201431b669dcf859a8f631a49cfdce43
Dylan Beattie <dylan@dylanbeattie.net>
2024-10-12 19:32:09 +0100

    fix: quine tests shouldn't be enabled yet. Oops.

--- Starship/Rockstar.Test/ParserTests.cs
+++ Starship/Rockstar.Test/ParserTests.cs
@@ -8,17 +8,6 @@ public class ParserTests(ITestOutputHelper output) : FixtureBase(output) {
 	public void Parse(RockFile file) => TestParser(file);
 }
 
-public class QuineTests(ITestOutputHelper testOutput) : RockstarTestBase(testOutput) {
-	[Theory]
-	[InlineData("quine")]
-	[InlineData("this is a quine")]
-	[InlineData("shout quine")]
-	public void Quines(string program) {
-		var (_, output) = RunProgram(program);
-		output.ShouldBe(program);
-	}
-}
-
 public class ArgumentTests {
 	[Theory]
 	[InlineData("arguments")]
---
+++ Starship/Rockstar.Test/QuineTests.cs
@@ -0,0 +1,12 @@
+namespace Rockstar.Test;
+
+public class QuineTests(ITestOutputHelper testOutput) : RockstarTestBase(testOutput) {
+	//[Theory]
+	//[InlineData("quine")]
+	//[InlineData("this is a quine")]
+	//[InlineData("shout quine")]
+	//public void Quines(string program) {
+	//	var (_, output) = RunProgram(program);
+	//	output.ShouldBe(program);
+	//}
+}

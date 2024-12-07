98fe6feb240e3568bbd8d7085124b51d19d5086b
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-07 22:04:40 +0000

    Memoize normalized variable name to improve performance

--- Starship/Rockstar.Engine/Expressions/Variable.cs
+++ Starship/Rockstar.Engine/Expressions/Variable.cs
@@ -4,7 +4,10 @@ using System.Text.RegularExpressions;
 namespace Rockstar.Engine.Expressions;
 
 public abstract class Variable(string name) : Expression {
+
 	public string Name => name;
+
+	protected string NormalizedName { get; init; } = String.Join("_", whitespace.Split(name));
 
 	public override string ToString()
 		=> $"{GetType().Name.ToLowerInvariant()}: {Key}"
@@ -28,8 +31,6 @@ public abstract class Variable(string name) : Expression {
 
 	private static readonly Regex whitespace = new("\\s+", RegexOptions.Compiled);
 
-	protected string NormalizedName
-		=> String.Join("_", whitespace.Split(Name));
 
 	public abstract string Key { get; }
 
